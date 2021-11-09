5f1ff43b8cd1b3a9cbb1cc24c9216edc9eb5eefd
Josh Olloqui <josholloqui@outlook.com>
2021-11-08 19:52:43 -0800

    added tailwind css to storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-red-200">Hello World</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
